c8cf52b209274877737e30520aa171bf6d9823fd
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-04 20:43:34 +0000

    update post_detail() and url

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,15 @@ def post_list(request):
     return render(request=request, template_name='blog/post/list.html', context={'posts': posts})
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
 
     return render(request=request, template_name='blog/post/detail.html', context={'post': post})
