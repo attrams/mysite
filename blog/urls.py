c715c0ce3bc2bee99647930eb0687791ae1c4ea6
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-06 20:40:18 +0000

    add feed url

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -14,5 +16,6 @@ urlpatterns = [
         name='post_detail'
     ),
     path('<int:post_id>/share/', view=views.post_share, name='post_share'),
-    path('<int:post_id>/comment/', view=views.post_comment, name='post_comment')
+    path('<int:post_id>/comment/', view=views.post_comment, name='post_comment'),
+    path('feed/', LatestPostsFeed(), name='post_feed')
 ]
